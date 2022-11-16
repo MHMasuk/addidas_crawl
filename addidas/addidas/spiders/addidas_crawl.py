044d294aba8c7c8e7db5d389ea2b22e5cf4a2555
MHMAsuk <mhmasuk.personal@gmail.com>
2022-11-16 17:44:35 +0600

    Add chrome driver

--- addidas/addidas/spiders/addidas_crawl.py
+++ addidas/addidas/spiders/addidas_crawl.py
@@ -3,8 +3,8 @@ import scrapy
 
 class AddidasCrawlSpider(scrapy.Spider):
     name = 'addidas_crawl'
-    allowed_domains = ['addidas.com']
-    start_urls = ['http://addidas.com/']
+    allowed_domains = ['shop.adidas.jp']
+    start_urls = ['https://shop.adidas.jp/men/']
 
     def parse(self, response):
         pass
